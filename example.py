39043d03aaad2ae2e7aec53db144a6e83fd0b359
rustamov <a.rustamov@icloud.com>
2020-07-07 10:57:44 +0200

    new comments

--- example.py
+++ example.py
@@ -1,5 +1,15 @@
 """
 Authors: B.Friman, A.Rustamov
+
+The formalism and analytic formulas used in the package are based on the publication:
+
+Peter Braun-Munzinger, Bengt Friman, Krzysztof Redlich, Anar Rustamov, Johanna Stachel
+Relativistic nuclear collisions: Establishing the non-critical baseline for fluctuation measurements.
+
+arXiv:2007.02463 [nucl-th]
+
+If you use the code to produce results for a publication, we ask you to be fair and cite the above paper!
+
 """
 
 from ce_cumulants.cumulans import Cumulants
